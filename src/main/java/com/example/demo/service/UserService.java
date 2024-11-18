ac66b7fb46e81a0bf79c32803c1790f34a142e5d
heejae369 <heejae369@gmail.com>
2024-11-18 11:15:09 +0900

    JDBC Repository로 이관 및 close시 exception 처리

--- src/main/java/com/example/demo/service/UserService.java
+++ src/main/java/com/example/demo/service/UserService.java
@@ -2,19 +2,29 @@ package com.example.demo.service;
 
 import com.example.demo.controller.dto.UserResponseDto;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import org.springframework.web.server.ResponseStatusException;
+import java.sql.SQLException;
 
 @Service
 @RequiredArgsConstructor
 public class UserService {
     private final UserRepository userRepository;
+    private final UserJdbcApiDao userJdbcRepository;
 
     public UserResponseDto findById(Integer id) {
-        User user = userRepository.findById(id);
-        return UserResponseDto.from(user);
+//        User user = userRepository.findById(id);
+//        return UserResponseDto.from(user);
+        try {
+            User user = userJdbcRepository.findById(id);
+            return UserResponseDto.from(user);
+        } catch (SQLException e) {// close도 SQLException로 발생, checked Exception로 처리, ResponseStatusException로
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "자원 반납 시 문제가 있습니다.");
+        }
     }
 
     public List<UserResponseDto> findAll() {
