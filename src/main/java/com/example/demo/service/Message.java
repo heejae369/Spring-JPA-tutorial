a7ad6dce00b18b9ceb3c72864c8f864359f1d8f2
heejae369 <heejae369@gmail.com>
2024-11-18 16:18:35 +0900

    message entity 및 JdbcApiDao 생성

---
+++ src/main/java/com/example/demo/service/Message.java
@@ -0,0 +1,19 @@
+package com.example.demo.service;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
+import java.time.LocalDateTime;
+@Getter
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Message {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private Integer userId;
+    private String message;
+    private LocalDateTime createdAt;
+}
