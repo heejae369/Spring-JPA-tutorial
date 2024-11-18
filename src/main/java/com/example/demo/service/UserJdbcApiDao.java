6a3b92d6f7e625190488cfe3bd20d2777c157d20
heejae369 <heejae369@gmail.com>
2024-11-18 11:12:06 +0900

    JDBC Driver 생성

---
+++ src/main/java/com/example/demo/service/UserJdbcApiDao.java
@@ -0,0 +1,54 @@
+package com.example.demo.service;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Repository;
+import org.springframework.web.server.ResponseStatusException;
+import java.sql.*;
+import java.time.ZoneId;
+@Slf4j
+@Repository
+public class UserJdbcApiDao {
+    @Value("${spring.datasource.url}") //Value : application.property에 있는 환경변수(우리가 정해놓은 것들)로 접속
+    private String url;
+    @Value("${spring.datasource.username}")
+    private String username;
+    @Value("${spring.datasource.password}")
+    private String password;
+    public User findById(int userId) throws SQLException {
+        Connection connection = null;   // 1, 접속
+        Statement statement = null;     // 2, 쿼리
+        ResultSet resultSet = null;     // 3, 쿼리 결과값
+        // DriverManager :
+        try {
+            connection = DriverManager.getConnection(   // 1
+                url, username, password
+            );
+            statement = connection.createStatement();   // 2
+            resultSet = statement.executeQuery(         // 3
+                "SELECT * FROM \"user\" WHERE id = " + userId
+            );
+            if (resultSet.next()) {// next() : 결과값 있냐
+                return new User(
+                    resultSet.getInt("id"),
+                    resultSet.getString("name"),
+                    resultSet.getInt("age"),
+                    resultSet.getString("job"),
+                    resultSet.getString("specialty"),
+                    resultSet.getTimestamp("created_at")
+                        .toInstant()
+                        .atZone(ZoneId.systemDefault())
+                        .toLocalDateTime()
+                );
+            }
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "유저 정보가 존재하지 않습니다 - id : " + userId);
+        } catch (SQLException e) {
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "자원에 대한 접근에 문제가 있습니다.");
+        } finally {// 다끝나고 난후, 실패 성공여부 상관없이
+            // 자원반납, 커넥션 닫음
+            if (resultSet != null) resultSet.close();   // 1
+            if (statement != null) statement.close();   // 2
+            if (connection != null) connection.close(); // 3
+        }
+    }
+}
