97f6b2e87bb6ddadac41b12c76e3b0aee91444be
heejae369 <heejae369@gmail.com>
2024-11-18 14:19:59 +0900

    PrepareStatement 적용 하여 유저 삽입

--- src/main/java/com/example/demo/service/UserJdbcApiDao.java
+++ src/main/java/com/example/demo/service/UserJdbcApiDao.java
@@ -13,6 +13,9 @@ import org.springframework.web.server.ResponseStatusException;
 import java.sql.*;
 import java.time.ZoneId;
 import javax.sql.DataSource;
+import java.time.LocalDateTime;
+
+
 @Slf4j
 @Repository
 @RequiredArgsConstructor
@@ -82,6 +85,62 @@ public class UserJdbcApiDao {
                 );
             }
             return results;
+        } catch (SQLException e) {
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "자원에 대한 접근에 문제가 있습니다.");
+        } finally {
+            // 자원반납
+            if (resultSet != null) resultSet.close();   // 1
+            if (statement != null) statement.close();   // 2
+            if (connection != null) connection.close(); // 3
+        }
+    }
+
+
+    public User save(String name, Integer age, String job, String specialty) throws SQLException {
+        Connection connection = null;           // 1
+        PreparedStatement statement = null;     // 2
+        ResultSet resultSet = null;             // 3
+        try {
+            connection = dataSource.getConnection();    // 1
+            // (A) INSERT USER
+            statement = connection.prepareStatement(    // (A)-2:Statement
+                "INSERT INTO \"user\" (name, age, job, specialty, created_at) VALUES (?, ?, ?, ?, ?)"
+            );
+            statement.setString(1, name);
+            statement.setInt(2, age);
+            statement.setString(3, job);
+            statement.setString(4, specialty);
+            statement.setTimestamp(5, Timestamp.valueOf(LocalDateTime.now()));
+            int executedNumberOfQuery = statement.executeUpdate();
+            // (B) SELECT id - MySQL:last_insert_id()->id / PostgresQL:currval()->lastval/lastval()->lastval
+            statement = connection.prepareStatement(    // (B)-2:Statement
+                "SELECT lastval()"
+            );
+            resultSet = statement.executeQuery();       // (B)-3:ResultSet
+            Integer createdUserId = null;
+            if (resultSet.next()) {
+                createdUserId = resultSet.getInt("lastval");
+            }
+            // (C) SELECT USER
+            statement = connection.prepareStatement(    // (C)-2:Statement
+                "SELECT * FROM \"user\" WHERE id = ?"
+            );
+            statement.setInt(1, createdUserId);
+            resultSet = statement.executeQuery();       // (C)-3:ResultSet
+            if (resultSet.next()) {
+                return new User(
+                    resultSet.getInt("id"),
+                    resultSet.getString("name"),
+                    resultSet.getInt("age"),
+                    resultSet.getString("job"),
+                    resultSet.getString("specialty"),
+                    resultSet.getTimestamp("created_at")
+                        .toInstant()
+                        .atZone(ZoneId.systemDefault())
+                        .toLocalDateTime()
+                );
+            }
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "유저 정보가 저장되지 않았습니다 - id : " + createdUserId);
         } catch (SQLException e) {
             throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "자원에 대한 접근에 문제가 있습니다.");
         } finally {
